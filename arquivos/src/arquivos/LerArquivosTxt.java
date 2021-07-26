94584220a502e0977ffcc7d5be8fc7d828a2f613
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-26 12:38:45 -0300

    16.3 - Lendo arquivo TXT e criando lista de objetos

--- arquivos/src/arquivos/LerArquivosTxt.java
+++ arquivos/src/arquivos/LerArquivosTxt.java
@@ -3,6 +3,8 @@ package arquivos;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivosTxt {
@@ -15,13 +17,26 @@ public class LerArquivosTxt {
 
 		Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
 		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
 		while (lerArquivo.hasNext()) {
 			String linha = lerArquivo.nextLine();
 			
 			if(linha != null && !linha.isEmpty()) {
-				System.out.println(linha);
+				String[] dados = linha.split("\\;");
+				
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNome(dados[0]);
+				pessoa.setEmail(dados[1]);
+				pessoa.setIdade(Integer.parseInt(dados[2]));
+				
+				pessoas.add(pessoa);
 			}
 			
+		}
+		
+		for (Pessoa pessoa : pessoas) {
+			System.out.println(pessoa);
 		}
 	}
 
--- arquivos/src/arquivos/Pessoa.java
+++ arquivos/src/arquivos/Pessoa.java
@@ -24,6 +24,10 @@ public class Pessoa {
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", email=" + email + ", idade=" + idade + "]";
+	}
 	
 	
 
